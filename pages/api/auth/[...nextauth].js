efb64f7916036b0890697eedf7ece02659389f44
swk23c8 <35516289+swk23C8@users.noreply.github.com>
2022-04-20 20:00:26 +0900

    changed magic email verification to async function

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -42,90 +42,97 @@ const emailsDir = path.resolve(process.cwd(), 'emails');
 //   });
 // };
 
-const sendVerificationRequest = ({ identifier, url }) => {
-  const emailFile = readFileSync(path.join(emailsDir, 'confirm-email.html'), {
-    encoding: 'utf8',
-  });
-  const emailTemplate = Handlebars.compile(emailFile);
-  sendgrid.send({
-    from: `"✨ Next-Casino" ${process.env.EMAIL_FROM}`,
-    to: identifier,
-    subject: 'Your sign-in link for Next-Casino',
-    html: emailTemplate({
-      base_url: process.env.NEXTAUTH_URL,
-      signin_url: url,
-      email: identifier,
-    }),
-  });
-};
-
-// const sendWelcomeEmail = async ({ user }) => {
-//   const { email } = user;
-
-//   try {
-//     const emailFile = readFileSync(path.join(emailsDir, 'welcome.html'), {
-//       encoding: 'utf8',
-//     });
-//     const emailTemplate = Handlebars.compile(emailFile);
-//     await transporter.sendMail({
-//       from: `"✨ Next-Casino" ${process.env.EMAIL_FROM}`,
-//       to: email,
-//       subject: 'Welcome to Next-Casino! 🎉',
-//       html: emailTemplate({
-//         base_url: process.env.NEXTAUTH_URL,
-//         support_email: 'next-casino@riseup.net',
-//       }),
-//     });
-//   } catch (error) {
-//     console.log(`❌ Unable to send welcome email to user (${email})`);
-//   }
-// };
-
-const sendWelcomeEmail = async ({ user }) => {
-  const { email } = user;
-
+const sendVerificationRequest = async ({ identifier, url }) => {
   try {
-    const emailFile = readFileSync(path.join(emailsDir, 'welcome.html'), {
+    const emailFile = readFileSync(path.join(emailsDir, 'confirm-email.html'), {
       encoding: 'utf8',
     });
     const emailTemplate = Handlebars.compile(emailFile);
     await sendgrid.send({
-      to: email,
-      // from: `"✨ Next-Casino" ${process.env.EMAIL_FROM}`,
+      to: identifier,
       from: {
         email: "next-casino-no-reply@riseup.net",
         name: "✨ Next-Casino"
       },
-      subject: 'Welcome to Next-Casino! 🎉',
+      subject: 'Your sign-in link for Next-Casino',
       html: emailTemplate({
         base_url: process.env.NEXTAUTH_URL,
-        support_email: 'next-casino@riseup.net',
+        signin_url: url,
+        email: identifier,
       }),
     });
   } catch (error) {
-    console.log(`❌ Unable to send welcome email to user (${email})`);
+    console.log(`❌ Unable to send sign-in magic email to user (${email})`);
     console.log(error);
-  }
-};
+  };
 
-export default NextAuth({
-  pages: {
-    signIn: '/',
-    signOut: '/',
-    error: '/',
-    verifyRequest: '/',
-  },
-  providers: [
-    EmailProvider({
-      maxAge: 10 * 60,
-      sendVerificationRequest,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  adapter: PrismaAdapter(prisma),
-  events: { createUser: sendWelcomeEmail },
-});
+  // const sendWelcomeEmail = async ({ user }) => {
+  //   const { email } = user;
+
+  //   try {
+  //     const emailFile = readFileSync(path.join(emailsDir, 'welcome.html'), {
+  //       encoding: 'utf8',
+  //     });
+  //     const emailTemplate = Handlebars.compile(emailFile);
+  //     await transporter.sendMail({
+  //       from: `"✨ Next-Casino" ${process.env.EMAIL_FROM}`,
+  //       to: email,
+  //       subject: 'Welcome to Next-Casino! 🎉',
+  //       html: emailTemplate({
+  //         base_url: process.env.NEXTAUTH_URL,
+  //         support_email: 'next-casino@riseup.net',
+  //       }),
+  //     });
+  //   } catch (error) {
+  //     console.log(`❌ Unable to send welcome email to user (${email})`);
+  //   }
+  // };
+
+  const sendWelcomeEmail = async ({ user }) => {
+    const { email } = user;
+
+    try {
+      const emailFile = readFileSync(path.join(emailsDir, 'welcome.html'), {
+        encoding: 'utf8',
+      });
+      const emailTemplate = Handlebars.compile(emailFile);
+      await sendgrid.send({
+        to: email,
+        // from: `"✨ Next-Casino" ${process.env.EMAIL_FROM}`,
+        from: {
+          email: "next-casino-no-reply@riseup.net",
+          name: "✨ Next-Casino"
+        },
+        subject: 'Welcome to Next-Casino! 🎉',
+        html: emailTemplate({
+          base_url: process.env.NEXTAUTH_URL,
+          support_email: 'next-casino@riseup.net',
+        }),
+      });
+    } catch (error) {
+      console.log(`❌ Unable to send welcome email to user (${email})`);
+      console.log(error);
+    }
+  };
+
+  export default NextAuth({
+    pages: {
+      signIn: '/',
+      signOut: '/',
+      error: '/',
+      verifyRequest: '/',
+    },
+    providers: [
+      EmailProvider({
+        maxAge: 10 * 60,
+        sendVerificationRequest,
+      }),
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+      }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    adapter: PrismaAdapter(prisma),
+    events: { createUser: sendWelcomeEmail },
+  });
