7fbaaa68c8b43fc7697373f318f781ac5122e652
swk23c8 <35516289+swk23C8@users.noreply.github.com>
2022-04-21 01:41:56 +0900

    .

--- pages/stats.js
+++ pages/stats.js
@@ -57,7 +57,7 @@ export default function Stat({ stats = [] }) {
 				{/* <Grid homes={homes} /> */}
 				<Table stats={userStats} />
 				{/* {console.log(userStats)} */}
-				{userStats.userType === 'admin' ? (
+				{userStats.userType === 'ADMIN' ? (
 					<>
 						<button
 							className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-1"
