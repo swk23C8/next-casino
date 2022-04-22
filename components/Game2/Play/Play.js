46efe6745f16e813a7dab59cb3cc122e02a7fbf6
swk23c8 <35516289+swk23C8@users.noreply.github.com>
2022-04-23 05:43:34 +0900

    adding major big shit

--- components/Game2/Play/Play.js
+++ components/Game2/Play/Play.js
@@ -38,7 +38,7 @@ const Play = ({ stats = [], game = [] }) => {
 	const makeBet = (e) => {
 		e.preventDefault();
 		axios.patch('/api/gameAction/makeBet', {
-			bet: e.target.bet.value*1
+			bet: e.target.bet.value * 1
 		})
 			.then(res => {
 				setPBet(res.data.pBet);
@@ -54,88 +54,243 @@ const Play = ({ stats = [], game = [] }) => {
 	return (
 		<>
 			<p>game is started</p>
-
-			{/* container to show dice */}
-			<div className="dice-container">
-				<p>Banker Dice</p>
-				<Dice refs={[bRef1, bRef2, bRef3, bDie_1, bDie_2, bDie_3]} />
-				<button onClick={() => {
-					bRef1.current.rollDice();
-					bRef2.current.rollDice();
-					bRef3.current.rollDice();
-				}}>
-					Roll Banker Dice
-				</button>
-				<p>Player Dice</p>
-				<Dice refs={[pRef1, pRef2, pRef3, pDie_1, pDie_2, pDie_3]} />
-				<button onClick={() => {
-					pRef1.current.rollDice();
-					pRef2.current.rollDice();
-					pRef3.current.rollDice();
-				}}>
-					Roll Player Dice
-				</button>
-			</div>
-
-			{/* container  to show ID, Token amount, Bet amount form with Bet button and Game Result*/}
-			<div className="game-container">
-				<div className="game-info">
-					<p>ID: {game.id}</p>
-					<p>Token Amount: {game.tokenAmount}</p>
-					<p>Bet Amount: {pBet}</p>
-					<p>Game Result: {game.result}</p>
+			<div className="grid min-h-max it grid-cols-3 grid-rows-2 gap-2 w-auto h-auto">
+				{/* container to show dice */}
+				<div className="dice-container box row-start-1 row-span-1">
+					<p>Banker Dice</p>
+					<Dice refs={[bRef1, bRef2, bRef3, bDie_1, bDie_2, bDie_3]} />
+					<button onClick={() => {
+						bRef1.current.rollDice();
+						bRef2.current.rollDice();
+						bRef3.current.rollDice();
+					}}>
+						Roll Banker Dice
+					</button>
+					<p>Player Dice</p>
+					<Dice refs={[pRef1, pRef2, pRef3, pDie_1, pDie_2, pDie_3]} />
+					<button onClick={() => {
+						pRef1.current.rollDice();
+						pRef2.current.rollDice();
+						pRef3.current.rollDice();
+					}}>
+						Roll Player Dice
+					</button>
 				</div>
-				<div className="game-form">
-					<p>Bet</p>
-					<form
-						onSubmit={
-							makeBet
-						}
-					// className={ }
-					>
-						<label>
-							Bet:
-							<input type="number" name="bet" />
-						</label>
-						<input
-							// className={styles.betButton}
-							type="submit"
-							value="BET"
-						/>
-					</form>
+
+				{/* container  to show ID, Token amount, Bet amount form with Bet button and Game Result*/}
+				<div className="game-container box row-start-1 row-span-1 col-start-2 col-end-3">
+					<div className="game-info">
+						<p>ID: {game.id}</p>
+						<p>Token Amount: {game.tokenAmount}</p>
+						<p>Bet Amount: {pBet}</p>
+						<p>Game Result: {game.result}</p>
+					</div>
+					<div className="game-form">
+						<p>Bet</p>
+						<form
+							onSubmit={
+								makeBet
+							}
+						// className={ }
+						>
+							<label>
+								Bet:
+								<input type="number" name="bet" />
+							</label>
+							<input
+								// className={styles.betButton}
+								type="submit"
+								value="BET"
+							/>
+						</form>
+						<p>HOTKEYS</p>
+						<p>SPACE: bet</p>
+						<p>E: halve bet</p>
+						<p>R: double bet</p>
+						<p>D: roll dice</p>
+					</div>
+				</div>
+				<div className="game-rules box row-start-1 row-span-2 col-start-3 col-end-4">
+					<table className="w-full shadow-md">
+						<thead>
+							<tr>
+								<th className="bg-blue-700 text-white p-1 w-auto">Name</th>
+								<th className="bg-blue-700 text-white p-1 w-auto">Example</th>
+								<th className="bg-blue-700 text-white p-1 w-auto">Outcome</th>
+								<th className="bg-blue-700 text-white p-1 w-auto">Comment</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr className="bg-blue-100 text-black">
+								<td className="">Triple</td>
+								<td className="">
+									<Image
+										src="https://cdn-icons-png.flaticon.com/512/142/142306.png"
+										alt="Die_face_6b"
+										// layout='fill'
+										width={35}
+										height={35}
+									/>
+									<Image
+										src="https://cdn-icons-png.flaticon.com/512/142/142306.png"
+										alt="Die_face_6b"
+										// layout='fill'
+										width={35}
+										height={35}
+									/>
+									<Image
+										src="https://cdn-icons-png.flaticon.com/512/142/142306.png"
+										alt="Die_face_6b"
+										// layout='fill'
+										width={35}
+										height={35}
+									/>
+								</td>
+								<td className="">WIN</td>
+								<td className="">All three dice show the same number.<br /> 6-6-6 is the highest roll,<br />5-5-5 is the next highest, etc.<br />Any triple is an instant win.</td>
+							</tr>
+							<tr className="bg-blue-200 text-black">
+								<td className="">4-5-6</td>
+								<td className="">
+									<Image
+										src="https://cdn-icons-png.flaticon.com/512/142/142304.png"
+										alt="Die_face_6b"
+										// layout='fill'
+										width={35}
+										height={35}
+									/>
+									<Image
+										src="https://cdn-icons-png.flaticon.com/512/142/142305.png"
+										alt="Die_face_6b"
+										// layout='fill'
+										width={35}
+										height={35}
+									/>
+									<Image
+										src="https://cdn-icons-png.flaticon.com/512/142/142306.png"
+										alt="Die_face_6b"
+										// layout='fill'
+										width={35}
+										height={35}
+									/>
+								</td>
+								<td className="">WIN</td>
+								<td className="">Sequential 4-5-6;<br /> this is an instant win.</td>
+							</tr>
+							<tr className="bg-blue-100 text-black">
+								<td className="">Points</td>
+								<td className="">
+									<Image
+										src="https://cdn-icons-png.flaticon.com/512/142/142304.png"
+										alt="Die_face_6b"
+										// layout='fill'
+										width={35}
+										height={35}
+									/>
+									<Image
+										src="https://cdn-icons-png.flaticon.com/512/142/142304.png"
+										alt="Die_face_6b"
+										// layout='fill'
+										width={35}
+										height={35}
+									/>
+									<Image
+										src="https://cdn-icons-png.flaticon.com/512/142/142305.png"
+										alt="Die_face_6b"
+										// layout='fill'
+										width={35}
+										height={35}
+									/>
+								</td>
+								<td className="">n points</td>
+								<td className="">
+									One pair plus any other value;<br /> the odd die is the point value.<br /> Possible point values range from 2 to 5<br />When the point value is 6, it is an instant win; <br />When the point value is 1, it is an instant loss.
+								</td>
+							</tr>
+							<tr className="bg-blue-200 text-black">
+								<td className="">1-2-3</td>
+								<td className="">
+									<Image
+										src="https://cdn-icons-png.flaticon.com/512/142/142301.png"
+										alt="Die_face_6b"
+										// layout='fill'
+										width={35}
+										height={35}
+									/>
+									<Image
+										src="https://cdn-icons-png.flaticon.com/512/142/142302.png"
+										alt="Die_face_6b"
+										// layout='fill'
+										width={35}
+										height={35}
+									/>
+									<Image
+										src="https://cdn-icons-png.flaticon.com/512/142/142303.png"
+										alt="Die_face_6b"
+										// layout='fill'
+										width={35}
+										height={35}
+									/>
+								</td>
+								<td className="">LOSE</td>
+								<td className="">Sequential 1-2-3;<br /> this is an instant loss.</td>
+							</tr>
+							<tr className="bg-blue-100 text-black">
+								<td className="">Indeterminate</td>
+								<td className="">
+									<Image
+										src="https://cdn-icons-png.flaticon.com/512/142/142303.png"
+										alt="Die_face_6b"
+										// layout='fill'
+										width={35}
+										height={35}
+									/>
+									<Image
+										src="https://cdn-icons-png.flaticon.com/512/142/142305.png"
+										alt="Die_face_6b"
+										// layout='fill'
+										width={35}
+										height={35}
+									/>
+									<Image
+										src="https://cdn-icons-png.flaticon.com/512/142/142306.png"
+										alt="Die_face_6b"
+										// layout='fill'
+										width={35}
+										height={35}
+									/>
+								</td>
+								<td className="">Re-Roll</td>
+								<td className="">
+									Any combination that does not result in a <br />triple, sequential, or points is an <br />indeterminate outcome and requires a re-roll.
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					add feature to get more coins/ads/captcha/something? here
+				</div>
+				<div className="box row-start-2 row-span-1 col-start-1 col-end-3">
+					<div className="grid overflow-hidden grid-cols-12 grid-rows-6 gap-1">
+						<div className="box2 row-start-1 row-span-1 col-start-1 col-span-12">1</div>
+						<div className="box2 row-start-2 row-span-3 col-start-1 col-span-1">2</div>
+						<div className="box2 row-start-5 row-span-3 col-start-1 col-span-1">3</div>
+						<div className="box2 row-start-2 row-span-6 col-start-2 col-span-11">4</div>
+						<div className="box2 row-start-8 row-span-1 col-start-1 col-span-11">5</div>
+						<div className="box2 row-start-8 row-span-1 col-start-12 col-span-1">6</div>
+					</div>
+					<div className="grid overflow-hidden grid-cols-12 grid-rows-6 gap-1">
+						<div className="box2 row-start-1 row-span-1 col-start-1 col-span-12">1</div>
+						<div className="box2 row-start-2 row-span-3 col-start-1 col-span-1">2</div>
+						<div className="box2 row-start-5 row-span-3 col-start-1 col-span-1">3</div>
+						<div className="box2 row-start-2 row-span-6 col-start-2 col-span-11">4</div>
+						<div className="box2 row-start-8 row-span-1 col-start-1 col-span-11">5</div>
+						<div className="box2 row-start-8 row-span-1 col-start-12 col-span-1">6</div>
+					</div>
+					<div className="grid overflow-hidden grid-cols-12 grid-rows-3 gap-1">
+						<div className="box2 row-start-2 row-span-3 col-start-1 col-span-1">2</div>
+					</div>
 				</div>
 			</div>
-
-			{/* button to add token amount by 100 */}
-			<button onClick={() => {
-				// fetch('/api/gameAction/addToken', {
-				// 	method: 'POST',
-				// 	headers: {
-				// 		'Content-Type': 'application/json',
-				// 	},
-				// 	body: JSON.stringify({
-				// 		userId: stats.id,
-				// 		tokenAmount: 100,
-				// 	}),
-				// })
-				// 	.then(res => {
-				// 		res.json()
-				// 		console.log(res)
-				// 	})
-				// 	.then(data => {
-				// 		console.log(data);
-				// 		if (data.error) {
-				// 			alert(data.error);
-				// 		} else {
-				// 			setGameState(data.game);
-				// 			setUserStats(data.stats);
-				// 		}
-				// 	})
-				// 	.catch(err => console.log(err));
-			}}>
-				Add 100 Tokens
-			</button>
-
 		</>
 	);
 }
