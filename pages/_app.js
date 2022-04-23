edacda880611fcd6d17cab9c1849e12ac71b3177
swk23c8 <35516289+swk23C8@users.noreply.github.com>
2022-04-24 04:59:29 +0900

    implement anti-adblock

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
 import '../styles/globals.css';
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider as AuthProvider } from 'next-auth/react';
+import { useDetectAdBlock } from "adblock-detect-react";
+import React from "react";
+
+
+const SomeFunctionalComponent = () => {
+  const adBlockDetected = useDetectAdBlock();
+
+  React.useEffect(() => {
+    if (adBlockDetected) {
+      window.alert("Please disable AdBlock! We need it to work!");
+    }
+  }, [adBlockDetected]);
+
+  return <div>{adBlockDetected && "Hello Ad Blocked Page"}</div>;
+};
 
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
@@ -8,6 +23,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
     <>
       <AuthProvider session={session}>
         <Component {...pageProps} />
+        {SomeFunctionalComponent()}
       </AuthProvider>
 
       <Toaster />
