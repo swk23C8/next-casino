ca020797b0415ce3e55975c1fa79678d05440981
swk23c8 <35516289+swk23C8@users.noreply.github.com>
2022-04-27 17:19:47 +0900

    refactored all rings into single reuseable Reel function

--- components/Slots/PixiApp/PixiApp.js
+++ components/Slots/PixiApp/PixiApp.js
@@ -92,11 +92,11 @@ const RotatingBunny = () => {
 	);
 };
 
-const Ring1 = () => {
-	const radius = 200;
-	const numberOfSectors = 13;
-	const radiansPerSector = 2 * Math.PI / numberOfSectors;
+const Reel = (param) => {
+	const radius = param.radius;
+	const numberOfSectors = param.numberOfSectors;
 
+	const radiansPerSector = 2 * Math.PI / numberOfSectors;
 	const textAnchorPercentage = (radius - 40 / 2) / radius;
 
 	const randomColour = () => {
@@ -116,7 +116,7 @@ const Ring1 = () => {
 			graphics.lineTo(0, 0);
 			graphics.endFill();
 		}
-	}, [color, numberOfSectors, radiansPerSector]);
+	}, [color, numberOfSectors, radiansPerSector, radius]);
 
 
 	const drawReelText = React.useCallback(() => {
@@ -126,7 +126,7 @@ const Ring1 = () => {
 			const rotation = sector * radiansPerSector
 			reelText.push(
 				<Text
-					key={"reel1_" + sector}
+					key={sector}
 					text={text}
 					anchor={(0.5, 0.5)}
 					// anchor={(0, 0)}
@@ -151,304 +151,7 @@ const Ring1 = () => {
 			);
 		}
 		return reelText;
-	}, [radiansPerSector, textAnchorPercentage]);
-
-	return (
-		<>
-			<Graphics
-				draw={drawReel}
-				cursor="not-allowed"
-				interactive={true}
-			/>
-			{drawReelText()}
-		</>
-	);
-}
-
-
-const Ring2 = () => {
-	const radius = 250;
-	const numberOfSectors = 13;
-	const radiansPerSector = 2 * Math.PI / numberOfSectors;
-
-	const textAnchorPercentage = (radius - 40 / 2) / radius;
-
-	const randomColour = () => {
-		return `0x${Math.floor(Math.random() * 16777215).toString(16)}`;
-	}
-	const color = randomColour();
-
-	const drawReel = React.useCallback((graphics) => {
-		for (let sector = 1; sector <= numberOfSectors; sector++) {
-			graphics.beginFill(color);
-			graphics.lineStyle(/*border width*/2,/*border color*/ 0xFFBD01, 1);
-			// graphics.lineStyle(/*border width*/2,/*border color*/ 0xffffff, 1);
-			const startingAngle = sector * radiansPerSector - radiansPerSector / 2;
-			const endingAngle = startingAngle + radiansPerSector;
-			graphics.moveTo(0, 0);
-			graphics.arc(0, 0, radius, startingAngle, endingAngle);
-			graphics.lineTo(0, 0);
-			graphics.endFill();
-		}
-	}, [color, numberOfSectors, radiansPerSector]);
-
-
-	const drawReelText = React.useCallback(() => {
-		const reelText = [];
-		for (let sector = 1; sector <= numberOfSectors; sector++) {
-			const text = sector.toString();
-			const rotation = sector * radiansPerSector
-
-			reelText.push(
-				<Text
-					key={"reel2_" + sector}
-					text={text}
-					anchor={(0.5, 0.5)}
-					// anchor={(0, 0)}
-					position={
-						[0 + (radius - 10) * textAnchorPercentage * Math.cos(rotation),
-						0 + (radius - 10) * textAnchorPercentage * Math.sin(rotation)]
-					}
-					rotation={rotation + Math.PI}
-					pivot={[0, 0]}
-					style={{
-						fontSize: 20,
-						fill: 0xffffff,
-						fontFamily: "Arial",
-						align: "center",
-						dropShadow: true,
-						dropShadowColor: 0x000000,
-						dropShadowBlur: 4,
-						dropShadowAngle: Math.PI / 6,
-						dropShadowDistance: 6,
-					}}
-				/>
-			);
-		}
-		return reelText;
-	}, [radiansPerSector, textAnchorPercentage]);
-
-	return (
-		<>
-			<Graphics
-				draw={drawReel}
-				cursor="not-allowed"
-				interactive={true}
-			/>
-			{drawReelText()}
-		</>
-	);
-}
-const Ring3 = () => {
-	const radius = 300;
-	const numberOfSectors = 13;
-	const radiansPerSector = 2 * Math.PI / numberOfSectors;
-
-	const textAnchorPercentage = (radius - 40 / 2) / radius;
-
-	const randomColour = () => {
-		return `0x${Math.floor(Math.random() * 16777215).toString(16)}`;
-	}
-	const color = randomColour();
-
-	const drawReel = React.useCallback((graphics) => {
-		for (let sector = 1; sector <= numberOfSectors; sector++) {
-			graphics.beginFill(color);
-			graphics.lineStyle(/*border width*/2,/*border color*/ 0xFFBD01, 1);
-			// graphics.lineStyle(/*border width*/2,/*border color*/ 0xffffff, 1);
-			const startingAngle = sector * radiansPerSector - radiansPerSector / 2;
-			const endingAngle = startingAngle + radiansPerSector;
-			graphics.moveTo(0, 0);
-			graphics.arc(0, 0, radius, startingAngle, endingAngle);
-			graphics.lineTo(0, 0);
-			graphics.endFill();
-		}
-	}, [color, numberOfSectors, radiansPerSector]);
-
-
-	const drawReelText = React.useCallback(() => {
-		const reelText = [];
-		for (let sector = 1; sector <= numberOfSectors; sector++) {
-			const text = sector.toString();
-			const rotation = sector * radiansPerSector
-
-			reelText.push(
-				<Text
-					key={"reel3_" + sector}
-					text={text}
-					anchor={(0.5, 0.5)}
-					// anchor={(0, 0)}
-					position={
-						[0 + (radius - 10) * textAnchorPercentage * Math.cos(rotation),
-						0 + (radius - 10) * textAnchorPercentage * Math.sin(rotation)]
-					}
-					rotation={rotation + Math.PI}
-					pivot={[0, 0]}
-					style={{
-						fontSize: 20,
-						fill: 0xffffff,
-						fontFamily: "Arial",
-						align: "center",
-						dropShadow: true,
-						dropShadowColor: 0x000000,
-						dropShadowBlur: 4,
-						dropShadowAngle: Math.PI / 6,
-						dropShadowDistance: 6,
-					}}
-				/>
-			);
-		}
-		return reelText;
-	}, [radiansPerSector, textAnchorPercentage]);
-
-	return (
-		<>
-			<Graphics
-				draw={drawReel}
-				cursor="not-allowed"
-				interactive={true}
-			/>
-			{drawReelText()}
-		</>
-	);
-}
-
-
-const Ring4 = () => {
-	const radius = 350;
-	const numberOfSectors = 13;
-	const radiansPerSector = 2 * Math.PI / numberOfSectors;
-
-	const textAnchorPercentage = (radius - 40 / 2) / radius;
-
-	const randomColour = () => {
-		return `0x${Math.floor(Math.random() * 16777215).toString(16)}`;
-	}
-	const color = randomColour();
-
-	const drawReel = React.useCallback((graphics) => {
-		for (let sector = 1; sector <= numberOfSectors; sector++) {
-			graphics.beginFill(color);
-			graphics.lineStyle(/*border width*/2,/*border color*/ 0xFFBD01, 1);
-			// graphics.lineStyle(/*border width*/2,/*border color*/ 0xffffff, 1);
-			const startingAngle = sector * radiansPerSector - radiansPerSector / 2;
-			const endingAngle = startingAngle + radiansPerSector;
-			graphics.moveTo(0, 0);
-			graphics.arc(0, 0, radius, startingAngle, endingAngle);
-			graphics.lineTo(0, 0);
-			graphics.endFill();
-		}
-	}, [color, numberOfSectors, radiansPerSector]);
-
-
-	const drawReelText = React.useCallback(() => {
-		const reelText = [];
-		for (let sector = 1; sector <= numberOfSectors; sector++) {
-			const text = sector.toString();
-			const rotation = sector * radiansPerSector
-
-			reelText.push(
-				<Text
-					key={"reel4_" + sector}
-					text={text}
-					anchor={(0.5, 0.5)}
-					// anchor={(0, 0)}
-					position={
-						[0 + (radius - 10) * textAnchorPercentage * Math.cos(rotation),
-						0 + (radius - 10) * textAnchorPercentage * Math.sin(rotation)]
-					}
-					rotation={rotation + Math.PI}
-					pivot={[0, 0]}
-					style={{
-						fontSize: 20,
-						fill: 0xffffff,
-						fontFamily: "Arial",
-						align: "center",
-						dropShadow: true,
-						dropShadowColor: 0x000000,
-						dropShadowBlur: 4,
-						dropShadowAngle: Math.PI / 6,
-						dropShadowDistance: 6,
-					}}
-				/>
-			);
-		}
-		return reelText;
-	}, [radiansPerSector, textAnchorPercentage]);
-
-	return (
-		<>
-			<Graphics
-				draw={drawReel}
-				cursor="not-allowed"
-				interactive={true}
-			/>
-			{drawReelText()}
-		</>
-	);
-}
-
-const Ring5 = () => {
-	const radius = 400;
-	const numberOfSectors = 13;
-	const radiansPerSector = 2 * Math.PI / numberOfSectors;
-
-	const textAnchorPercentage = (radius - 40 / 2) / radius;
-
-	const randomColour = () => {
-		return `0x${Math.floor(Math.random() * 16777215).toString(16)}`;
-	}
-	const color = randomColour();
-
-	const drawReel = React.useCallback((graphics) => {
-		for (let sector = 1; sector <= numberOfSectors; sector++) {
-			graphics.beginFill(color);
-			graphics.lineStyle(/*border width*/2,/*border color*/ 0xFFBD01, 1);
-			// graphics.lineStyle(/*border width*/2,/*border color*/ 0xffffff, 1);
-			const startingAngle = sector * radiansPerSector - radiansPerSector / 2;
-			const endingAngle = startingAngle + radiansPerSector;
-			graphics.moveTo(0, 0);
-			graphics.arc(0, 0, radius, startingAngle, endingAngle);
-			graphics.lineTo(0, 0);
-			graphics.endFill();
-		}
-	}, [color, numberOfSectors, radiansPerSector]);
-
-
-	const drawReelText = React.useCallback(() => {
-		const reelText = [];
-		for (let sector = 1; sector <= numberOfSectors; sector++) {
-			const text = sector.toString();
-			const rotation = sector * radiansPerSector
-
-			reelText.push(
-				<Text
-					key={"reel5_" + sector}
-					text={text}
-					anchor={(0.5, 0.5)}
-					// anchor={(0, 0)}
-					position={
-						[0 + (radius - 10) * textAnchorPercentage * Math.cos(rotation),
-						0 + (radius - 10) * textAnchorPercentage * Math.sin(rotation)]
-					}
-					rotation={rotation + Math.PI}
-					pivot={[0, 0]}
-					style={{
-						fontSize: 20,
-						fill: 0xffffff,
-						fontFamily: "Arial",
-						align: "center",
-						dropShadow: true,
-						dropShadowColor: 0x000000,
-						dropShadowBlur: 4,
-						dropShadowAngle: Math.PI / 6,
-						dropShadowDistance: 6,
-					}}
-				/>
-			);
-		}
-		return reelText;
-	}, [radiansPerSector, textAnchorPercentage]);
+	}, [numberOfSectors, radiansPerSector, radius, textAnchorPercentage]);
 
 	return (
 		<>
@@ -468,7 +171,7 @@ const ReelButton = () => {
 	return (
 		<Container
 			pivot={[0, 0]}
-			position={[400, 400]}
+			position={[0, 0]}
 		>
 			<Graphics
 				draw={(ctx) => {
@@ -497,16 +200,28 @@ const PixiApp = () => {
 				position={[400, 400]}
 				pivot={[0, 0]}
 			>
-
-				<Ring5 />
-				<Ring4 />
-				<Ring3 />
-				<Ring2 />
-				<Ring1 />
-				{/* {ReelButton()} */}
-
+				<Reel
+					radius={400}
+					numberOfSectors={13}
+				/>
+				<Reel
+					radius={350}
+					numberOfSectors={13}
+				/>
+				<Reel
+					radius={300}
+					numberOfSectors={13}
+				/>
+				<Reel
+					radius={250}
+					numberOfSectors={13}
+				/>
+				<Reel
+					radius={200}
+					numberOfSectors={13}
+				/>
+				{ReelButton()}
 			</Container>
-			{ReelButton()}
 		</Stage>
 	);
 };
