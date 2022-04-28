a15790d8acf67cd6ae6ba820ecee31852353bc02
swk23c8 <35516289+swk23C8@users.noreply.github.com>
2022-04-29 04:16:13 +0900

    some basic privacy page, fixed typo in confirm-email.html, updated blocked.js function name

--- pages/blocked.js
+++ pages/blocked.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-export default function adFrame() {
+export default function blocked() {
 	return (
 		<>
 			<div className="blocked-inner">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   content: [
+    // Exclude /pages/privacy.js
+    '!./pages/privacy.js',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  // Exclude /pages/privacy.js
+  exclude: [
+    './pages/privacy.js',
+  ],
+
   theme: {
     extend: {},
   },
