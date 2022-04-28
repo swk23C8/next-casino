a76abd3a3b3f9a495974f6fc23258daebcb1c108
swk23c8 <35516289+swk23C8@users.noreply.github.com>
2022-04-29 02:58:21 +0900

    trying ads?

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,11 @@ import '../styles/globals.css';
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider as AuthProvider } from 'next-auth/react';
 import { useDetectAdBlock } from "adblock-detect-react";
-import { AdBlockDetectedWrapper } from "adblock-detect-react";
+// import { AdBlockDetectedWrapper } from "adblock-detect-react";
 import React from "react";
 import { useRouter } from 'next/router'
+import Script from "next/script";
+
 
 
 const SomeFunctionalComponent = () => {
@@ -28,10 +30,25 @@ const SomeFunctionalComponent = () => {
 };
 
 
+{/* <Script
+  async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4743953812411324"
+  crossorigin="anonymous" /> */}
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
+
+
+
       <AuthProvider session={session}>
+        <Script
+          id="Adsense-id"
+          data-ad-client="ca-pub-4743953812411324"
+          async="true"
+          strategy="afterInteractive"
+          onError={(e) => { console.error('Script failed to load', e) }}
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        />
         <Component {...pageProps} />
         {SomeFunctionalComponent()}
       </AuthProvider>
