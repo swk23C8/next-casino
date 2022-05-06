bad09507d908587252a926acbc2f28ab04789028
swk23C8 <35516289+swk23C8@users.noreply.github.com>
2022-05-06 21:01:10 +0900

    typo in import directory

--- packages/frontend/components/Test/Play/Play.js
+++ packages/frontend/components/Test/Play/Play.js
@@ -20,8 +20,8 @@ import {
 } from '@heroicons/react/outline';
 import { Box, Flex, Heading, Text, Center } from "@chakra-ui/layout";
 import { useEffect, useRef, useState } from "react";
-import { checkWinCon } from '@/components/Test/logic/WinCon'
-import { playerTwo } from '@/components/Test/logic/PlayerTwo';
+import { checkWinCon } from '@/components/Test/Logic/WinCon'
+import { playerTwo } from '@/components/Test/Logic/PlayerTwo';
 
 export default function Game({ gameMode, socket = null, inLobby = null }) {
 	const router = useRouter();
